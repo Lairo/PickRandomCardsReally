e5eeceacbbcddd7daf9d64dfc3c0dffb05f3f1fb
chajus1 <chajus1@googlemail.com>
2024-09-05 04:15:16 +0100

    Add project files.

---
+++ PickRandomCardsReally/CardPicker.cs
@@ -0,0 +1,38 @@
+﻿
+namespace PickRandomCardsReally
+{
+    internal class CardPicker
+    {
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + ReturnSuit();
+            }
+            return pickedCards;
+        }
+
+       // Random random = new Random();
+
+        private static string ReturnSuit()
+        {
+            int value = Random.Shared.Next(1, 5);
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = Random.Shared.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
---
+++ PickRandomCardsReally/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PickRandomCardsReally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Enter the number of cards to pick: ");
+                string? line = Console.ReadLine();
+                if (int.TryParse(line, out int numberOfCards))
+                {
+
+                    string[] cards = CardPicker.PickSomeCards(numberOfCards);
+
+                    foreach (string card in cards)
+                    {
+                        Console.WriteLine($"Your Cards: {card}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter Correct number please");
+                }
+                Console.WriteLine("\nPress x to exit or anything else to continue\n");
+                string bob = Console.ReadLine();
+                if (bob == "x") return;
+
+            }
+        }
+    }
+}
